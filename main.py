174db32577d393703cbdd49929b04c85b2138d51
Anđa Čavić <117020409+Beberoo@users.noreply.github.com>
2024-03-18 10:32:55 +0000

    drugi zadatak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 filmovi = [{"Naziv" : "Film1", "PozKomentari" : 5, "NegKomentari" :6}, {"Naziv" : "Film2", "PozKomentari" : 2, "NegKomentari" :18}, {"Naziv" : "Film3", "PozKomentari" : 12, "NegKomentari" : 4}]
-
-def film_najbolji(filmovi)
+def film_najbolji(filmovi):
     najBr = 0
     najFilm = ""
     for film in filmovi:
@@ -8,4 +7,19 @@ def film_najbolji(filmovi)
             najBr = film["PozKomentari"]
             najFilm = film["Naziv"]
     print(f"Najbolji film je {najFilm} i njegov broj pozitivnih kometara je {najBr}")
+    return
+
+def PocinjeSa (filmovi,pocetak):
+    for film in filmovi:
+        if film["Naziv"].startswith(pocetak):
+            print(film["Naziv"])
+    return
+
+pocetak = input("Unesite pocetak naziva koji zelite")
+
+print(PocinjeSa(filmovi,pocetak))
+
+
+
+
 
