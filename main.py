53ecb5467262a2a249d03d5edac47d791c7df1ba
Anđa Čavić <117020409+Beberoo@users.noreply.github.com>
2024-03-18 10:11:27 +0000

    prvi zadatak added

---
+++ main.py
@@ -0,0 +1,11 @@
+filmovi = [{"Naziv" : "Film1", "PozKomentari" : 5, "NegKomentari" :6}, {"Naziv" : "Film2", "PozKomentari" : 2, "NegKomentari" :18}, {"Naziv" : "Film3", "PozKomentari" : 12, "NegKomentari" : 4}]
+
+def film_najbolji(filmovi)
+    najBr = 0
+    najFilm = ""
+    for film in filmovi:
+        if film["PozKomentari"] > najBr:
+            najBr = film["PozKomentari"]
+            najFilm = film["Naziv"]
+    print(f"Najbolji film je {najFilm} i njegov broj pozitivnih kometara je {najBr}")
+
